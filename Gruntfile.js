b060a1148af5a53d35eb1fa2fdae285418e1156c
Ken Finnigan <ken@kenfinnigan.me>
2014-09-30 10:52:54 -0400

    Move htmlhint rules into separate config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,16 +66,7 @@ module.exports = function(grunt) {
         html: {
           src: ['src/**/*.html'],
           options: {
-            'tagname-lowercase': true,
-            'attr-lowercase': true,
-            'attr-value-doublequotes': true,
-            'tag-pair': true,
-            'tag-self-close': true,
-            'id-unique': true,
-            'src-not-empty': true,
-            'style-disabled': true,
-            'img-alt-require': true,
-            'spec-char-escape': true
+            htmlhintrc: '.htmlhintrc'
           }
         }
       },
