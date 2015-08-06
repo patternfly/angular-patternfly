9897640e8fccec38f5854414b1743e2f00da49c9
David Taylor <dtaylor@redhat.com>
2015-08-06 15:56:40 -0400

    Donut Chart: made thresholds user defined, added ng-docs, created unit test

--- src/charts/donut/donut-percent-chart-directive.js
+++ src/charts/donut/donut-percent-chart-directive.js
@@ -1,3 +1,61 @@
+/**
+ * @ngdoc directive
+ * @name patternfly.charts.directive:pfDonutPctChart
+ *
+ * @description
+ *   Directive for rendering a percentage used donut/radial chart.  The Used Percentage fill starts at 12 o’clock and
+ *   moves clockwise.  Whatever portion of the donut not Used, will be represented as Available, and rendered as a
+ *   gray fill.
+ *   There are three possible fill colors for Used Percentage, dependent on whether or not there are thresholds:<br/>
+ *   <ul>
+ *   <li>When no thresholds exist, or if the used percentage has not surpassed any thresholds, the indicator is blue.
+ *   <li>When the used percentage has surpassed the warning threshold, but not the error threshold, the indicator is orange.
+ *   <li>When the used percentage has surpassed the error threshold, the indicator is is red.
+ *   </ul>
+ *   The directive will calculate the Available Percentage (Total - Used), and display it as a grey radial fill.
+ *
+ * @param {object} config configuration settings for the donut chart:<br/>
+ * <strong>config</strong>
+ * <ul style='list-style-type: none'>
+ * <li>.chartId       - the ID iof the container that the chart should bind to
+ * <li>.totalUnits    - unit label for values, ex: 'MHz','GB', etc..
+ * <li>.usageDataName - suffix used in label inside of donut, ex: 'Used','Allocated', etc..
+ * <li>.thresholds    - warning and error percentage thresholds used to determine the Usage Percentage fill color
+ * </ul>
+ *
+ * @param {object} data the Total and Used values for the donut chart.  Available is calculated as Total - Used.<br/>
+ * <strong>data</strong>
+ * <ul style='list-style-type: none'>
+ * <li>.used   - number representing the amount used
+ * <li>.total  - number representing the total amount
+ * </ul>
+ *
+ * @example
+ <example module="patternfly.charts">
+ <file name="index.html">
+   <div ng-controller="ChartCtrl">
+     <div pf-donut-pct-chart config="config" data="data"></div>
+   </div>
+ </file>
+
+ <file name="script.js">
+   function ChartCtrl($scope) {
+        $scope.config = {
+           'chartId': 'cpuUsageChart',
+           'totalUnits': 'MHz',
+           'usageDataName': 'Used',
+           'thresholds':{'warning':'75.0','error':'90.00'}
+       };
+
+       $scope.data = {
+           'used': '950',
+           'total': '1000'
+       };
+
+   };
+ </file>
+ </example>
+ */
 angular.module('patternfly.charts').directive('pfDonutPctChart', ['ChartsMixin', '$timeout',
   function(chartsMixin, $timeout) {
     'use strict';
@@ -15,13 +73,20 @@ angular.module('patternfly.charts').directive('pfDonutPctChart', ['ChartsMixin',
           if ($scope.config.chartId) {
             $scope.donutChartId = $scope.config.chartId + $scope.donutChartId;
           }
-          $scope.getStatusColor = function(used) {
-            if (used >= 90.0) {
-              return '#CC0000';
-            } else if (used >= 75.0) {
-              return '#EC7A08';
-            } else {
+          if ($scope.data.available === undefined) {
+            $scope.data.available = $scope.data.total - $scope.data.used;
+          }
+          $scope.getStatusColor = function(used, thresholds) {
+            if (!thresholds){
               return '#0088CE';
+            } else {
+              if (used >= thresholds.error) {
+                return '#CC0000';
+              } else if (used >= thresholds.warning) {
+                return '#EC7A08';
+              } else {
+                return '#0088CE';
+              }
             }
           };
           $scope.statusDonutColor = function(scope) {
@@ -29,7 +94,7 @@ angular.module('patternfly.charts').directive('pfDonutPctChart', ['ChartsMixin',
               pattern: []
             };
             var percentUsed = scope.data.used / scope.data.total * 100.0;
-            color.pattern[0] = scope.getStatusColor(percentUsed);
+            color.pattern[0] = scope.getStatusColor(percentUsed, scope.config.thresholds);
             color.pattern[1] = '#D1D1D1';
             return color;
           };
@@ -68,6 +133,16 @@ angular.module('patternfly.charts').directive('pfDonutPctChart', ['ChartsMixin',
           scope.config.donutConfig.data = scope.getDonutData(scope);
           setupDonutChartTitle();
         });
+        attrs.$observe('donutConfig', function() {
+          scope.config.donutConfig = $.extend(true, angular.copy(scope.defaultDonutConfig), scope.config.donutConfig);
+          scope.config.donutConfig.data = scope.getDonutData(scope);
+          setupDonutChartTitle();
+        });
+        attrs.$observe('data', function() {
+          scope.config.donutConfig.data = scope.getDonutData(scope);
+          setupDonutChartTitle();
+        });
+
         var setupDonutChartTitle = function() {
           $timeout(function() {
             var donutChartTitle = element[0].querySelector('text.c3-chart-arcs-title');
