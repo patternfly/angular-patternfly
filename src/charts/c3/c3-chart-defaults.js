8df0e9bf875320b40e613de70f25c814c14c9d78
Jeffrey Phillips <jephilli@redhat.com>
2015-08-20 14:20:46 -0400

    Add Sparkline Chart Directive

--- src/charts/c3/c3-chart-defaults.js
+++ src/charts/c3/c3-chart-defaults.js
@@ -39,7 +39,65 @@
         color: this.getDefaultDonutColor(),
         tooltip: this.getDefaultDonutTooltip()
       };
+    },
+    getDefaultSparklineArea: function () {
+      return {
+        zerobased: true
+      };
+    },
+    getDefaultSparklineSize: function () {
+      return {
+        height: 60
+      };
+    },
+    getDefaultSparklineAxis: function () {
+      return {
+        x: {
+          show: false
+        },
+        y: {
+          show: false
+        }
+      };
+    },
+    getDefaultSparklineColor: function () {
+      return {
+        pattern: ['#0088ce', '#00659c', '#3f9c35', '#ec7a08', '#cc0000']
+      };
+    },
+    getDefaultSparklineLegend: function () {
+      return {
+        show: false
+      };
+    },
+    getDefaultSparklinePoint: function () {
+      return {
+        r: 1,
+        focus: {
+          expand: {
+            r: 4
+          }
+        }
+      };
+    },
+    getDefaultSparklineTooltip: function () {
+      return {
+        // because a sparkline should only contain a single data column, the tooltip will only work for a single data column
+        contents: function (d) {
+          return '<span class="c3-tooltip-sparkline">' + d[0].value + ' ' + d[0].name + '</span>';
+        }
+      };
+    },
+    getDefaultSparklineConfig: function () {
+      return {
+        area: this.getDefaultSparklineArea(),
+        size: this.getDefaultSparklineSize(),
+        axis: this.getDefaultSparklineAxis(),
+        color: this.getDefaultSparklineColor(),
+        legend: this.getDefaultSparklineLegend(),
+        point: this.getDefaultSparklinePoint(),
+        tooltip: this.getDefaultSparklineTooltip()
+      };
     }
   });
-
 })();
