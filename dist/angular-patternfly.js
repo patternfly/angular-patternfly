6da15ec2fc0f81b9c47a9b293f15b7b74e4e0c3d
David Taylor <dtaylor@redhat.com>
2015-10-29 15:10:38 -0400

    Fix for PTNFLY-539 "AngJS Simple Filter - Attr. Selector should be single select#"

--- dist/angular-patternfly.js
+++ dist/angular-patternfly.js
@@ -1999,7 +1999,7 @@ angular.module('patternfly.charts').directive('pfUtilizationChart', function () 
  * <li>.id          - (String) Optional unique Id for the filter field, useful for comparisons
  * <li>.title       - (String) The title to display for the filter field
  * <li>.placeholder - (String) Text to display when no filter value has been entered
- * <li>.filterType  - (String) The filter input field type (any html input type, or 'select' for a select box)
+ * <li>.filterType  - (String) The filter input field type (any html input type, or 'select' for a single select box)
  * <li>.filterValues - (Array) List of valid select values used when filterType is 'select'
  * </ul>
  * <li>.appliedFilters - (Array) List of the currently applied filters
@@ -2169,13 +2169,23 @@ angular.module('patternfly.filters').directive('pfSimpleFilter', function () {
         return foundFilter !== undefined;
       };
 
+      $scope.enforceSingleSelect = function (filter) {
+        _.remove($scope.config.appliedFilters, {title: filter.title});
+      };
+
       $scope.addFilter = function (field, value) {
         var newFilter = {
           id: field.id,
           title: field.title,
+          type: field.filterType,
           value: value
         };
         if (!$scope.filterExists(newFilter)) {
+
+          if (newFilter.type === 'select') {
+            $scope.enforceSingleSelect(newFilter);
+          }
+
           $scope.config.appliedFilters.push(newFilter);
 
           if ($scope.config.onFilterChange) {
