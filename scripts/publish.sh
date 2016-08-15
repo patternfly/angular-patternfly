2e505354a53f513a9afc75dfb4b3bdc1ab73f751
priley86 <priley86@gmail.com>
2016-08-15 13:00:37 -0400

    fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -29,7 +29,7 @@ then
   then
     git fetch upstream $TRAVIS_BRANCH-dist:$TRAVIS_BRANCH-dist
     git checkout $TRAVIS_BRANCH-dist
-    git merge $TRAVIS_BRANCH
+    git rebase $TRAVIS_BRANCH
     git push upstream $TRAVIS_BRANCH-dist --force -v
   else
     git push upstream $TRAVIS_BRANCH:$TRAVIS_BRANCH-dist --force -v
